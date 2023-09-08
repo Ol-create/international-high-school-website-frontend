59912af561f074367f7d3b8d384ef7ec164d1d40
Ol-create <oluola96@gmail.com>
2023-09-08 18:47:18 +0100

    add navbar top

---
+++ src/component/NavBar.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+
+export default function NavBar() {
+  const [isOpen, setOpen] = useState(false);
+
+  const handleMouseLeave = () => {
+    setOpen(false);
+  };
+
+  const handleMouseEnter = () => {
+    setOpen(true);
+  };
+
+  return (
+    <div>
+      <div className="flex navbar">
+        <div className="pr-5">
+          <h1> SINGAPORE AMERICAN SCHOOL </h1>
+        </div>
+        <div className="flex">
+          <img
+            alt=""
+            src="/images/safeguarding.png"
+            width={60}
+            height={20}
+          />
+          <img
+            src="/images/reimagined.png"
+            alt=""
+            width={65}
+            height={20}
+          />
+          <img
+            src="/images/menu-dropdown.png"
+            alt=""
+            width={30}
+            height={17}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+          />
+          <img
+            src="/images/search-icon.png"
+            alt=""
+            width={15}
+            height={15}
+          />
+        </div>
+      </div>
+      {isOpen && (
+        <div>
+          <ul>
+            <li>Students</li>
+            <li>Parents</li>
+            <li>Alumni</li>
+            <li>Employee</li>
+            <li>New Families</li>
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+}
+
